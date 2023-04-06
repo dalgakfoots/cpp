90c5a5544b9bc23a42822a7e2bd63a9530ee451c
dalgakfoots <hsw3074@gmail.com>
2023-04-06 12:15:27 +0900

    1_3 변수

--- TBCppStudy/Chapter1_3/main_chapter1_3.cpp
+++ TBCppStudy/Chapter1_3/main_chapter1_3.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
-    int x;
-    x = 123;
+    int x = 1; // initialization
+    x = 123; // assignment
+
+    std::cout << x << std::endl;
+    //std::cout << &x << std::endl;
 
     return 0;
 }
