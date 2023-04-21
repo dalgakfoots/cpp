061e6451289ea002a25e6c48517d8612f8ece7be
dalgakfoots <hsw3074@gmail.com>
2023-04-21 17:04:35 +0900

    feat) add repulsor beam

--- GameDevPracticePackage-master/Chapter1/Lecture1/WalkingPerson.h
+++ GameDevPracticePackage-master/Chapter1/Lecture1/WalkingPerson.h
@@ -1,35 +1,31 @@
 #pragma once
 
 #include "Game2D.h"
-
+#include <vector>
+#include <iostream>
 namespace jm
 {
-	/*
-	TODO:
-	- add left arm and left leg
-	- make a Person class and use it to draw many people.
-	- make an Ironman and allow for him to shoot repulsor beam with his right hand
-	*/
 
-	class WalkingPerson : public Game2D
+	class Person
 	{
 		float time = 0.0f;
 		vec2 init_vec;
+		bool action = false;
 
 	public :
-		WalkingPerson(vec2 in_vec) : init_vec(in_vec)
+		Person(vec2 in_vec) : init_vec(in_vec)
 		{
-			
 		}
 
-	public:
-		void update() override
+	public :
+		void draw()
 		{
+			beginTransformation();
 			translate(init_vec);
 
 			// gold face
 			beginTransformation();
-			translate(0.0f , 0.12f);
+			translate(0.0f, 0.12f);
 			drawFilledCircle(Colors::gold, 0.08f);
 			translate(0.05f, 0.03f);
 			drawFilledCircle(Colors::white, 0.01f); // while eye
@@ -53,7 +49,14 @@ namespace jm
 
 			// yellow arm
 			beginTransformation();
-			rotate(sin(time*0.25f) * 30.0f);					// animation!
+			if (action)
+			{
+				rotate(90.0f);					// animation!
+			} 
+			else
+			{
+				rotate(sin(time * 0.25f) * 30.0f);					// animation!
+			}
 			scale(1.0f, 2.0f);
 			translate(0.0f, -0.1f);
 			drawFilledBox(Colors::yellow, 0.05f, 0.18f);
@@ -63,7 +66,7 @@ namespace jm
 			beginTransformation();
 			translate(0.0f, -0.6f);
 			translate(0.0f, 0.2f);
-			rotate(sinf(time* 0.25f + 3.141592f) * 30.0f);	// animation!
+			rotate(sinf(time * 0.25f + 3.141592f) * 30.0f);	// animation!
 			translate(0.0f, -0.2f);
 			drawFilledBox(Colors::green, 0.1f, 0.4f);
 			endTransformation();
@@ -76,8 +79,95 @@ namespace jm
 			translate(0.0f, -0.2f);
 			drawFilledBox(Colors::green, 0.1f, 0.4f);
 			endTransformation();
+			endTransformation();
+		}
 
-			time += this->getTimeStep();
+		void update(const float& dt)
+		{
+			time += dt;
+		}
+
+		void toggleAction(const bool in_action)
+		{
+			action = in_action;
+		}
+	};
+
+
+	class RepulsorBeam
+	{
+		bool action = false;
+
+	public :
+
+		void draw()
+		{
+			if (action)
+			{
+				beginTransformation();
+				translate(vec2(0.8f, 0.0f));
+				drawFilledBox(Colors::skyblue, 1.0f, 1.0f);
+				endTransformation();
+				
+			}
+		}
+
+		void toggleAction(const bool in_action)
+		{
+			action = in_action;
+		}
+	};
+
+	/*
+	TODO:
+	- add left arm and left leg
+	- make a Person class and use it to draw many people.
+	- make an Ironman and allow for him to shoot repulsor beam with his right hand
+	*/
+
+	class WalkingPerson : public Game2D
+	{
+		//Person p{ vec2(0.5f , 0.5f) };
+		std::vector<Person> v_people;
+		RepulsorBeam b;
+	public :
+		WalkingPerson()
+		{
+			makePeople(3);
+		}
+	public:
+		void update() override
+		{
+			if (isKeyPressed(GLFW_KEY_SPACE))
+			{
+				v_people[0].toggleAction(true);
+				b.toggleAction(true);
+			}
+			else {
+				v_people[0].toggleAction(false);
+				b.toggleAction(false);
+			}
+
+
+			v_people[0].update(getTimeStep());
+			v_people[0].draw();
+			b.draw();
+		}
+
+		void makePeople(const unsigned in_u)
+		{
+			/*for (unsigned i = 0; i < in_u; i++)
+			{
+				Person p = Person{ vec2(in_u / 10 , in_u / 10) };
+				v_people.push_back(p);
+			}*/
+			Person p = Person{ vec2(0.0f , 0.0f) };
+			Person p1 = Person{ vec2(0.4f , 0.4f) };
+			Person p2 = Person{ vec2(-0.2f , -0.2f) };
+			v_people.push_back(p);
+			v_people.push_back(p1);
+			v_people.push_back(p2);
+
 		}
 	};
 }
